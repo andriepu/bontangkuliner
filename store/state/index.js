92eebf8090a4f32d4e122bf12c5a5a42d87ad3c7
andriepu <andriepu@gmail.com>
2020-04-24 06:02:58 +0700

    Fix shuffle array

--- store/state/index.js
+++ store/state/index.js
@@ -1,4 +1,4 @@
-import shuffle from 'lodash.shuffle';
+import shuffle from 'shuffle-array';
 
 export default {
   isSearch: false,
@@ -9,19 +9,23 @@ export default {
       href: 'https://toko.ly/dimsumjuarabtg',
       image: '/images/merchants/ayam-kremes-juara.jpg',
       text: 'Ayam Kremes Juara',
-    }, {
+    },
+    {
       href: 'https://toko.ly/dimsumjuarabtg',
       image: '/images/merchants/dimsum-juara.jpg',
       text: 'Dimsum Juara',
-    }, {
+    },
+    {
       href: undefined,
       image: '/images/merchants/takoichi.jpg',
       text: 'Takoichi',
-    }, {
+    },
+    {
       href: undefined,
       image: '/images/merchants/telor-gulung.jpg',
       text: 'Telor Gulung',
-    },{
+    },
+    {
       href: 'https://instagram.com/freshmilk.bontang?igshid=qewcssxkgixp',
       image: '/images/merchants/FRESHMILK_THAITEA_FA-02.png',
       name: 'FRESHMILK THAITEA',
@@ -33,11 +37,13 @@ export default {
       href: 'https://toko.ly/dimsumjuarabtg',
       image: '/images/merchants/ayam-kremes-juara.jpg',
       name: 'Ayam Kremes Juara',
-    }, {
+    },
+    {
       href: 'https://toko.ly/dimsumjuarabtg',
       image: '/images/merchants/dimsum-juara.jpg',
       name: 'Dimsum Juara',
-    }, {
+    },
+    {
       href: undefined,
       image: '/images/merchants/takoichi.jpg',
       name: 'Takoichi',
@@ -45,11 +51,12 @@ export default {
   ]),
 
   products: shuffle([
-     {
+    {
       href: undefined,
       image: '/images/merchants/telor-gulung.jpg',
       name: 'Telor Gulung',
-    },{
+    },
+    {
       href: 'https://instagram.com/freshmilk.bontang?igshid=qewcssxkgixp',
       image: '/images/merchants/FRESHMILK_THAITEA_FA-02.png',
       name: 'FRESHMILK THAITEA',
