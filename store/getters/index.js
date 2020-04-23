330bb4a4e8ef568a0db1abac6fec7521564935db
andriepu <andriepu@gmail.com>
2020-04-23 14:52:54 +0700

    Update correct link

--- store/getters/index.js
+++ store/getters/index.js
@@ -1,8 +1,10 @@
 export default {
   products (state) {
+    const products = [...state.topProducts, ...state.products];
+
     return !state.isSearch || !state.filter
-      ? state.products
-      : state.products.filter(product => (
+      ? products
+      : products.filter(product => (
         // new RegExp(`\\b(${state.filter.split(/\s+/).join('|')})\\b`, 'i')
         new RegExp(`(${state.filter.split(/\s+/).join('|')})`, 'i')
           .test(product.name)
